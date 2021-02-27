8cd8819eda65839afef132fa05eab6d0951f313f
Irsyaad Budi <irsyaadbp01@gmail.com>
2021-02-28 01:03:24 +0700

    Backend: add message success auth

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -51,6 +51,7 @@ export class AuthController {
 
     res.json({
       success: true,
+      message: "Register sucess",
       data: dataUser,
     });
   };
@@ -76,6 +77,7 @@ export class AuthController {
         });
         res.json({
           success: true,
+          message: "Login sucess",
           data: dataUser,
         });
       } else {
