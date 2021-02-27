aabb17f793feaa6aa8330e9f9dc7e960ba6badbb
Irsyaad Budi <irsyaadbp01@gmail.com>
2021-02-27 17:59:43 +0700

    add console log env

--- backend/src/config/ormconfig.ts
+++ backend/src/config/ormconfig.ts
@@ -3,6 +3,14 @@ import { ConnectionOptions } from "typeorm";
 dotenv.config();
 const SOURCE_PATH = process.env.NODE_ENV === "production" ? "dist" : "src";
 
+console.log({
+  host: process.env.PG_HOST,
+  port: 5432,
+  username: process.env.PG_USERNAME,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+});
+
 export const config = {
   name: "default",
   type: "postgres",
