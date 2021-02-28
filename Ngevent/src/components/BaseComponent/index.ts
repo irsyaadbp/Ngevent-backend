5ad3277842bae81c4367b17853df2ce2456856d8
Irsyaad Budi <irsyaadbp01@gmail.com>
2021-02-28 07:45:36 +0700

    frontend: change folder to mobile

--- Ngevent/src/components/BaseComponent/index.ts
+++
@@ -1,3 +0,0 @@
-export {default as Statusbar} from './Statusbar';
-export {default as Button} from './Button';
-export {default as LoadingBlock} from './LoadingBlock';
--- Ngevent/src/utils/common.ts
+++
@@ -1,8 +0,0 @@
-import {UserData} from '@ngevent/api/auth';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import {KEY_USER} from './constants';
-
-export async function getUserData(): Promise<UserData> {
-  const userData = await AsyncStorage.getItem(KEY_USER);
-  return typeof userData === 'string' ? JSON.parse(userData) : null;
-}
